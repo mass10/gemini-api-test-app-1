829de549f35c2ad49df68ed451895559b19f850a
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-03-02 12:09:47 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,10 @@
-use serde_json::json;
-use std::collections::HashMap;
+//!
+//! Rust + Gemini API
+//!
 
-#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug)]
-struct Part {
-	pub text: String,
-}
-
-#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug)]
-struct Content {
-	pub parts: Vec<Part>,
-	pub role: String,
-}
-
-#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug)]
-struct CitationSource {
-	#[serde(rename = "startIndex")]
-	pub start_index: i32,
-	#[serde(rename = "endIndex")]
-	pub end_index: i32,
-}
-
-#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug)]
-struct CitationMetadata {
-	#[serde(rename = "citationSources")]
-	pub citation_sources: Vec<CitationSource>,
-}
-
-#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug)]
-struct Candidate {
-	pub content: Content,
-	#[serde(rename = "finishReason")]
-	pub finish_reason: String,
-	#[serde(rename = "citationMetadata")]
-	pub citation_metadata: CitationMetadata,
-	#[serde(rename = "avgLogprobs")]
-	pub avg_logprobs: f64,
-}
-
-#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug)]
-struct TokenDetail {
-	pub modality: String,
-	#[serde(rename = "tokenCount")]
-	pub token_count: i32,
-}
-
-#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug)]
-struct UsageMetadata {
-	#[serde(rename = "promptTokenCount")]
-	pub prompt_token_count: i32,
-	#[serde(rename = "candidatesTokenCount")]
-	pub candidates_token_count: i32,
-	#[serde(rename = "totalTokenCount")]
-	pub total_token_count: i32,
-	#[serde(rename = "promptTokensDetails")]
-	pub prompt_tokens_details: Vec<TokenDetail>,
-	#[serde(rename = "candidatesTokensDetails")]
-	pub candidates_tokens_details: Vec<TokenDetail>,
-}
-
-#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug)]
-struct GeminiApiResponse {
-	pub candidates: Vec<Candidate>,
-	#[serde(rename = "usageMetadata")]
-	pub usage_metadata: UsageMetadata,
-	#[serde(rename = "modelVersion")]
-	pub model_version: String,
-}
+mod gemini_api;
+mod types;
+mod util;
 
 /// 環境変数を取得します。
 #[allow(unused)]
@@ -73,51 +12,10 @@ fn getenv(name: &str) -> String {
 	return std::env::var(name).unwrap_or_default();
 }
 
-/// テスト その2
-#[allow(unused)]
-fn test2() -> Result<(), Box<dyn std::error::Error>> {
-	let gemini_api_key = getenv("GEMINI_API_KEY");
-
-	// curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=GEMINI_API_KEY" \
-	// -H 'Content-Type: application/json' \
-	// -X POST \
-	// -d '{
-	//   "contents": [{
-	//     "parts":[{"text": "Explain how AI works"}]
-	//     }]
-	//    }'
-
-	let url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
-
-	let mut query = HashMap::<&str, String>::new();
-	query.insert("key", gemini_api_key);
-
-	let mut headers = reqwest::header::HeaderMap::new();
-	headers.insert("Content-Type", "application/json".parse().unwrap());
-
-	let body = json!({
-		"contents": [{
-			"parts":[{"text": "Explain how AI works"}]
-		}]
-	})
-	.to_string();
-
-	let client = reqwest::blocking::Client::new();
-	let res = client.post(url).query(&query).headers(headers).body(body).send()?.text()?;
-
-	println!("{}", res);
-
-	let gemini_response: GeminiApiResponse = serde_json::from_str(&res)?;
-
-	println!("{:?}", gemini_response);
-
-	return Ok(());
-}
-
 /// Rust アプリケーションのエントリーポイント
 #[tokio::main]
 async fn main() {
-	let result = test2();
+	let result = gemini_api::ask_gemini("Explain how AI works？ in Japanese please.");
 	if result.is_err() {
 		println!("Error: {:?}", result.err());
 		return;
